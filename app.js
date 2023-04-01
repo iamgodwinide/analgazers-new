5aa8d266ca03768dee6fd88bc5c0ca88e0a10806
Godwin Idemudia <godwinide@gmail.com>
2023-04-01 10:08:40 +0100

    new

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-fart = new Fart();
+const fart = new Fart();
+const popupContainer = document.querySelector(".popup");
+const popupContent = document.querySelector(".popup .content");
+
+const audio = new Audio();
 
 const sounds = [
     "toot",
@@ -15,10 +19,22 @@ const sounds = [
     "eine",
     "fartception",
     "fartpoint1"
-]
+];
 
-const playFart = () => {
-    fart.play(sounds[Math.floor(Math.random() * 13)])
+const writeup = [
+    'Once upon a time in the land of Rearopolis, there was a peculiar yet captivating character known as "The Gazed." Forever in pursuit of the most amusing and cheeky sights, he dedicated his life to uncovering the secrets of the enigmatic world of Anal Gazers.',
+    "One fateful day, The Gazed stumbled upon a mysterious portal shaped like a giant, grinning derriere. Unable to resist the allure, he leaped through the opening and found himself swallowed by a colossal beast known as the Gluteus Gobbler.",
+    "Navigating through the winding digestive tract, The Gazed encountered a vibrant assortment of NFTs, each more hilarious and entertaining than the last. He couldn't help but marvel at the colorful and comical images that filled this peculiar realm",
+    "As he reached the end of his bizarre journey, The Gazed was met with the ultimate treasure trove: a gallery of the finest and most amusing Anal Gazers NFTs ever discovered. Overwhelmed by their hilarity, he let out a hearty laugh that echoed through the beast's cavernous insides.",
+    "The Gluteus Gobbler, tickled by the sound of The Gazed's laughter, couldn't help but join in the merriment. As the laughter grew louder and more intense, the beast finally erupted in a fit of giggles, propelling The Gazed out into the open air with a comedic toot.",
+    "Landing safely on the ground, The Gazed couldn't help but chuckle as he recalled his incredible adventure. He vowed to share his discoveries with the world, offering a glimpse into the side-splitting universe of the Anal Gazers NFT project.",
+    "And so, with a twinkle in his eye and a grin on his face, The Gazed embarked on a mission to spread laughter and mirth throughout the land, forever championing the humor and artistry of the wacky world he'd discovered within the belly of the Gluteus Gobbler."
+];
+
+
+const playFart = (num) => {
+    fart.play(sounds[Math.floor(Math.random() * 13)]);
+    showpopup(num);
 }
 
 const enterFunc = () => {
@@ -36,6 +52,25 @@ const enterFunc = () => {
         });
     }
 }
+
+const showpopup = async (num) => {
+    if(typeof num === 'number'){
+        popupContainer.style.display = "flex"
+        popupContent.classList.add("content-animate");
+        popupContent.querySelector("p").innerText = writeup[num]
+        audio.pause();
+        audio.src = `./sounds/New gazed${num+1}.mp3`;
+        audio.play();
+    }
+}
+
+const hidepopup = () => {
+    audio.pause();
+    popupContainer.style.display = "none"
+    popupContent.classList.remove("content-animate");
+}
+
+popupContainer.addEventListener("click", hidepopup);
 
 window.onload = () => {
     window.scrollTo({
